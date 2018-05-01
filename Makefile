cc22bacf3714e5bb6ff1980c690a7e576b45d3e5
ClementGuittat <32964240+ClementGuittat@users.noreply.github.com>
2018-05-01 18:27:17 +0200

    first commit with makefile and main

---
+++ Makefile
@@ -0,0 +1,29 @@
+.SUFFIXES :
+.PHONY : clean,mrproper
+
+CXX = g++
+FDMAP = -c
+FLAG = -pedantic -Wall -std=c++11
+
+test : main.o DoctorManager.o Doctor.o PrintManager.o Print.o Attribute.o QualitativeAttribute.o QuantitativeAttribute.o
+	$(CXX) $(FLAG) $^ -o $@ -g
+
+
+main.o : models/DoctorManager.h
+DoctorManager.o : models/Doctor.h
+PrintManager.o : models/Print.h
+Print.o : models/Attribute.h
+
+main.o : main.cpp
+	$(CXX) $(FDMAP) $(FLAG) main.cpp
+
+%.o : models/%.cpp
+	$(CXX) $(FDMAP) $(FLAG) models/$*.cpp
+
+#make clean pour supprimer les fichiers temporaires
+clean :
+	rm -rf *.o
+
+#make mrproper && make pour supprimer avant de reconstruire 
+mrproper : clean
+	rm -rf Programme
---
+++ main.cpp
@@ -0,0 +1,41 @@
+//
+//  main.cpp
+//  ProjectUML
+//
+//  Created by Clement Guittat on 30/04/2018.
+//  Copyright © 2018 Clement Guittat. All rights reserved.
+//
+
+#include <iostream>
+#include "./models/DoctorManager.h"
+#include "./models/PrintManager.h"
+#include "./models/QualitativeAttribute.h"
+#include "./models/QuantitativeAttribute.h"
+
+int main(int argc, const char * argv[]) {
+    DoctorManager dm = DoctorManager();
+    dm.createDoctor("Charles", "Dupond", "chirurgien");
+    dm.createDoctor("Pierre", "Goutu", "neuro");
+    /*list<Doctor*> ld = dm.getDoctors();
+    for(ListDoctor::iterator it=ld.begin(); it!=ld.end(); ++it)
+    {
+        cout << *(*it);
+    }*/
+    //cout << *dm.getDoctor(2);
+    list<Attribute*> la;
+    Attribute* A1 = new QualitativeAttribute("A1", "OUI");
+    Attribute* A2 = new QuantitativeAttribute("A2", 2.2);
+    la.push_back(A1);
+    la.push_back(A2);
+    PrintManager pm = PrintManager();
+    pm.createPrint(la, "-");
+    pm.createPrint(la, "M1");
+    list <Print *> lp = pm.getPrints();
+    for(list <Print *>::iterator it=lp.begin(); it!=lp.end(); ++it)
+    {
+        cout << *(*it);
+    }
+    //cout << *pm.getPrint(2);
+    
+    return 0;
+}
